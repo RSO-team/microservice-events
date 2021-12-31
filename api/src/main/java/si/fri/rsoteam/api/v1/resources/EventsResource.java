2b4d17b115b757e2a1a6ec017428ddea0b40ad6a
creativ10 <mv5195@student.uni-lj.si>
2021-12-31 05:49:10 +0100

    added annotations for open-api. Fix dependencies

--- api/src/main/java/si/fri/rsoteam/api/v1/resources/EventsResource.java
+++ api/src/main/java/si/fri/rsoteam/api/v1/resources/EventsResource.java
@@ -34,29 +34,65 @@ public class EventsResource {
     protected UriInfo uriInfo;
 
     @GET
+    @Operation(summary = "Return all events.", description = "Returns the list of all the events.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully returned all events",
+                    responseCode = "200",
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )
+    })
     public Response getObjects() {
         return Response.ok(eventsBean.getList()).build();
     }
 
     @GET
     @Path("/{objectId}")
+    @Operation(summary = "Return event by id.", description = "Returns specific event by id.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully returned specific event",
+                    responseCode = "200",
+            )
+    })
     public Response getEventById(@PathParam("objectId") Integer id) {
         return Response.ok(eventsBean.getEvent(id)).build();
     }
 
     @POST
+    @Operation(summary = "Create new event.", description = "Create new event.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully created new event",
+                    responseCode = "201",
+            )
+    })
     public Response createEvent(EventDto eventDto) {
         return Response.status(201).entity(eventsBean.createEvent(eventDto)).build();
     }
 
     @PUT
     @Path("{objectId}")
+    @Operation(summary = "Update event.", description = "Update specific event by id.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully updated new event",
+                    responseCode = "201",
+            )
+    })
     public Response updateEvent(@PathParam("objectId") Integer id, EventDto eventDto) {
         return Response.status(201).entity(eventsBean.updateEvent(eventDto, id)).build();
     }
 
     @DELETE
     @Path("{objectId}")
+    @Operation(summary = "Delete an event.", description = "Delete specific event.")
+    @APIResponses({
+            @APIResponse(
+                    description = "Successfully deleted event",
+                    responseCode = "204",
+            )
+    })
     public Response deleteEvent(@PathParam("objectId") Integer id) {
         eventsBean.deleteEvent(id);
         return Response.status(204).build();
