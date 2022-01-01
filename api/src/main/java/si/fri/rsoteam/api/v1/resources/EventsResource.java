058ecaf218af3a32d7c4ee7629741ae282cc499b
Luka Galjot <luka.galjot@cloud.si>
2022-01-01 20:22:34 +0100

    Add logs and interceptor

--- api/src/main/java/si/fri/rsoteam/api/v1/resources/EventsResource.java
+++ api/src/main/java/si/fri/rsoteam/api/v1/resources/EventsResource.java
@@ -1,5 +1,9 @@
 package si.fri.rsoteam.api.v1.resources;
 
+import com.kumuluz.ee.logs.LogManager;
+import com.kumuluz.ee.logs.Logger;
+import com.kumuluz.ee.logs.cdi.Log;
+import com.kumuluz.ee.logs.cdi.LogParams;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.headers.Header;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
@@ -21,7 +25,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.util.logging.Logger;
 
 @ApplicationScoped
 @Path("/events")
@@ -29,7 +32,7 @@ import java.util.logging.Logger;
 @Consumes(MediaType.APPLICATION_JSON)
 public class EventsResource {
 
-    private Logger log = Logger.getLogger(EventsResource.class.getName());
+    private Logger log = LogManager.getLogger(EventsResource.class.getName());
 
     @Inject
     private EventsBean eventsBean;
@@ -46,6 +49,7 @@ public class EventsResource {
                     headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
             )
     })
+    @Log(LogParams.METRICS)
     public Response getObjects() {
         return Response.ok(eventsBean.getList()).build();
     }
@@ -59,6 +63,7 @@ public class EventsResource {
                     responseCode = "200"
             )
     })
+    @Log(LogParams.METRICS)
     public Response getEventById(@PathParam("objectId") Integer id) {
         return Response.ok(eventsBean.getEvent(id)).build();
     }
@@ -71,6 +76,7 @@ public class EventsResource {
                     responseCode = "201"
             )
     })
+    @Log(LogParams.METRICS)
     public Response createEvent(EventDto eventDto) {
         return Response.status(201).entity(eventsBean.createEvent(eventDto)).build();
     }
@@ -84,6 +90,7 @@ public class EventsResource {
                     responseCode = "201"
             )
     })
+    @Log(LogParams.METRICS)
     public Response updateEvent(@PathParam("objectId") Integer id, EventDto eventDto) {
         return Response.status(201).entity(eventsBean.updateEvent(eventDto, id)).build();
     }
@@ -97,6 +104,7 @@ public class EventsResource {
                     responseCode = "204"
             )
     })
+    @Log(LogParams.METRICS)
     public Response deleteEvent(@PathParam("objectId") Integer id) {
         eventsBean.deleteEvent(id);
         return Response.status(204).build();
